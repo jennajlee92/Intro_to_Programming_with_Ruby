2ceb1920a5ffe45e07419f91cad221f75a3c89a7
Jenna Ji Min Lee <my email address used on my git repo>
2022-03-21 14:54:12 -0700

    Added exercises file

---
+++ methods/exercises.rb
@@ -0,0 +1,18 @@
+def greeting(name)
+  p "Hello, #{name}!"
+end
+
+greeting("Jenna")
+
+def multiply(a, b)
+  a * b
+end
+
+p multiply(3, 4)
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
