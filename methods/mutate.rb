b4bc181b0e0126eddeb8f8a6aa75159d3c228fd2
Jenna Lee <my email address used on my git repo>
2022-03-21 13:56:56 -0700

    Added mutate and return files

--- methods/mutate.rb
+++ methods/mutate.rb
@@ -19,3 +19,13 @@ end
 p "Before no_mutate method: #{a}"
 no_mutate(a)
 p "After no_mutate method: #{a}"
+
+a = [1, 2, 3]
+
+def mutate(array)
+  array.pop
+end
+
+p "Before mutate method: #{a}"
+p mutate(a)
+p "After mutate method: #{a}"
