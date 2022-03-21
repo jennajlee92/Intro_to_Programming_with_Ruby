751a8d4354d4179b2db6feac57fb62969cba944a
Jenna Lee <jennalee92@Jennas-MBP.lan>
2022-03-21 13:41:02 -0700

    Added new files to method chapter

--- methods/say.rb
+++ methods/say.rb
@@ -1,7 +1,11 @@
-def say(words)
+def say(words='hello')
   puts words + "."
 end
 
+# Parentheses are optional in Ruby; be careful to distinguish btwn local var and method names!
+
+say
+say "hi"
 say("hello")
 say("hi")
 say("how are you")
