47c197760352e61e0b51be01c8889174d55fee2b
Jenna <55537982+jennajlee92@users.noreply.github.com>
2022-03-23 14:44:33 -0700

    Added exception examples file

---
+++ exception_example.rb
@@ -0,0 +1,24 @@
+names = ['bob', 'joe', 'steve', nil, 'frank']
+
+names.each do |name|
+  begin
+    puts "#{name}'s name has #{name.length} letters in it."
+  rescue
+    puts "Something went wrong!"
+  end
+end
+
+zero = 0
+puts "Before each call"
+zero.each { |element| puts element } rescue puts "Can't do that!"
+puts "After each call"
+
+def divide(number, divisor)
+  begin
+    answer = number / divisor
+  rescue ZeroDivisionError => e
+    puts e.message
+  end
+end
+
+puts divide(4, 0)
